8434d474e23c74cc1e646178fe11ee2d2254332b
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2023-09-11 00:00:41 +0100

    updated

--- src/components/Projectlist.jsx
+++ src/components/Projectlist.jsx
@@ -1,11 +1,11 @@
 import ProjectCard from "../components/Projectcard";
 import { Container, Row, Col } from "react-bootstrap";
-import adminpanel from '/src/assets/images/adminpanel.jpg'
-import olximage from "/src/assets/images/olximage.jpg";
-import netfliximage from "/src/assets/images/netflix.jpg";
-import gridlinesbuilders from "/src/assets/images/Gridlinesbuilders.jpg";
-import charlespizza from "/src/assets/images/pizza.jpg";
-import todolist from "/src/assets/images/todolist.jpg";
+import adminpanel from '/src/Assets/images/adminpanel.jpg'
+import olximage from "/src/Assets/images/olximage.jpg";
+import netfliximage from "/src/Assets/images/netflix.jpg";
+import gridlinesbuilders from "/src/Assets/images/Gridlinesbuilders.jpg";
+import charlespizza from "/src/Assets/images/pizza.jpg";
+import todolist from "/src/Assets/images/todolist.jpg";
 import "../pages/style.css";
 
 
