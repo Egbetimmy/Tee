2d226a73fe1bb3419db4a1e3a0bfe112bce8dad4
Egbe Timmy <egbetiimmy@gmail.com>
2024-02-13 19:09:54 +0000

    updated

--- src/components/Projectlist.jsx
+++ src/components/Projectlist.jsx
@@ -70,6 +70,26 @@ const projectsData = [
     tags: ['React', 'Frontend', 'TMDB API'],
     task: 'Backend',
   },
+  
+  {
+    imgPath: todolist,
+    isBlog: false,
+    title: 'To Do List',
+    description: 'To-Do App that build will allow a user to add a task to a list of to-do items. Once the task is added, the user will be able to delete it as completed once it has done.',
+    ghLink: 'https://todolist-rahul81.netlify.app/',
+    tags: ['React', 'Frontend', 'TMDB API'],
+    task: 'Backend',
+  },
+  
+  {
+    imgPath: todolist,
+    isBlog: false,
+    title: 'To Do List',
+    description: 'To-Do App that build will allow a user to add a task to a list of to-do items. Once the task is added, the user will be able to delete it as completed once it has done.',
+    ghLink: 'https://todolist-rahul81.netlify.app/',
+    tags: ['React', 'Frontend', 'TMDB API'],
+    task: 'Backend',
+  },
   // Add other project data here...
 ];
 
